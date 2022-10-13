c007e9c876d80fdb69937ddce3ffeaa4ec2f8eb5
Semen Trofimov <semiony@gmail.com>
2022-10-13 16:54:54 +0300

    add leson0

--- Main.go
+++ Main.go
@@ -1,25 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
-	"net/http"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/", helloworld)
-	e := http.ListenAndServe(":9090", nil)
-	if e != nil {
-		fmt.Println(e.Error())
-	}
-}
+	seconds := time.Now().Unix()
+	rand.Seed(seconds)
+	target := rand.Intn(100) + 1
+	fmt.Println("i ve chosen a random number between 1 and 100.")
+	fmt.Println("Can you gues it?")
+	fmt.Println(target)
 
-func helloworld(w http.ResponseWriter, r *http.Request) {
-	str := "hello"
-	n, e := io.WriteString(w, str)
-	if e != nil {
-		fmt.Println(e.Error())
-	} else {
-		fmt.Println(n, "", len(str))
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Make a guess: ")
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	input = strings.TrimSpace(input)
+	guess, err := strconv.Atoi(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if guess < target {
+		fmt.Println("OOps. Your gues was LOW.")
+	} else if guess > target {
+		fmt.Println("OOps. Your guess was HIGHT.")
+
 	}
 }
