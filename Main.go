81e72b450b018b0c3b415668bf00b00398604166
Semen Trofimov <semiony@gmail.com>
2022-10-13 17:05:31 +0300

    test

--- Main.go
+++ Main.go
@@ -1,39 +1,9 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"math/rand"
-	"os"
-	"strconv"
-	"strings"
-	"time"
-)
+import "fmt"
 
 func main() {
-	seconds := time.Now().Unix()
-	rand.Seed(seconds)
-	target := rand.Intn(100) + 1
-	fmt.Println("i ve chosen a random number between 1 and 100.")
-	fmt.Println("Can you gues it?")
-	fmt.Println(target)
-
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Make a guess: ")
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
-	input = strings.TrimSpace(input)
-	guess, err := strconv.Atoi(input)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if guess < target {
-		fmt.Println("OOps. Your gues was LOW.")
-	} else if guess > target {
-		fmt.Println("OOps. Your guess was HIGHT.")
-
+	for x := 1; true; x += 10 {
+		fmt.Println(x)
 	}
 }
