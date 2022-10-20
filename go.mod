f42334167267b55db165d7beddccb24a22b7cec8
Semen Trofimov <semiony@gmail.com>
2022-10-20 13:12:29 +0300

    add

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
-module horovod-center
+module importing
 
-go 1.16
-
-require github.com/gorilla/mux v1.8.0 // indirect
+go 1.18
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"golang-lessons/greeting"
+)
+
+func main() {
+	fmt.Println(greeting.Get)
+}
